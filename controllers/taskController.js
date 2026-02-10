77c4dc1b3b52929d519615ff86ffb92ab3194617
unknown <bhuvansaketh123@gmail.com>
2026-02-10 22:27:09 +0530

    backend

---
+++ controllers/taskController.js
@@ -0,0 +1,25 @@
+const Task = require("../models/Task");
+
+// Create Task
+exports.createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json(task);
+};
+
+// Get Tasks
+exports.getTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+};
+
+// Update Task
+exports.updateTask = async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(task);
+};
+
+// Delete Task
+exports.deleteTask = async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task Deleted" });
+};
